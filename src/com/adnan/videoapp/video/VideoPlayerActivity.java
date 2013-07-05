54e59d56f231d834fd8e3d4c275160c1f7773fed
Adnan Haris <adnan_haris@neovasolutions.com>
2013-07-05 16:39:18 +0530

    Playing video through file chooser.
    	modified:   src/com/adnan/videoapp/video/VideoPlayerActivity.java

--- src/com/adnan/videoapp/video/VideoPlayerActivity.java
+++ src/com/adnan/videoapp/video/VideoPlayerActivity.java
@@ -6,11 +6,14 @@ import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.adnan.videoapp.R;
 
 public class VideoPlayerActivity extends Activity implements OnPreparedListener, OnCompletionListener{
+	
+	VideoView mVideoPlayer;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -18,28 +21,29 @@ public class VideoPlayerActivity extends Activity implements OnPreparedListener,
 		setContentView(R.layout.video_player_layout);
 		System.out.println("Data:  " + getIntent().getExtras().getString("VIDEO_URI"));
 		Uri videoUri = Uri.parse(getIntent().getExtras().getString("VIDEO_URI"));
-		VideoView mVideoPlayer = (VideoView) findViewById(R.id.video_videoView);
-		//MediaController mc = new MediaController(getApplicationContext());
-		//mVideoPlayer.setMediaController(mc); //
+		mVideoPlayer = (VideoView) findViewById(R.id.video_videoView);
+		MediaController mc = new MediaController(getApplicationContext());
+		mVideoPlayer.setMediaController(mc); /* To get controls for a MediaPlayer */
+		mc.show();
 		mVideoPlayer.setKeepScreenOn(true); /* Set screen on */
 		mVideoPlayer.setVideoURI(videoUri);
 		mVideoPlayer.setOnPreparedListener(this);
 		mVideoPlayer.setOnCompletionListener(this);
 		
-		mVideoPlayer.start();
-	}
-
-	@Override
-	public void onCompletion(MediaPlayer mp) {
-		// TODO Auto-generated method stub
-		
+		//mVideoPlayer.start();
 	}
 
 	@Override
 	public void onPrepared(MediaPlayer mp) {
-		// TODO Auto-generated method stub
-		
+		 if(mVideoPlayer.canSeekForward()) mVideoPlayer.seekTo(mVideoPlayer.getDuration()/5);
+	        mVideoPlayer.start();		
 	}
+	
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		this.finish();
+	}
+
 	
 
 }
